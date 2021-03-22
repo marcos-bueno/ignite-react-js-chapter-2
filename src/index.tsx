8d6f216f5fa715b07a3e7de7d34d39f406490cb6
Marcos <mabueno@protonmail.com>
2021-03-22 15:29:40 -0300

    listing transactions and seeds

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,28 @@ createServer({
   models: {
     transaction: Model,
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelancer de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 600,
+          createdAt: new Date("2021-03-22 15:20:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2021-03-28 11:00:00"),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
 
